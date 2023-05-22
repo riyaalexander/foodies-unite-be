a9d55dc4e97463df739c4384f919f261e6073534
riyaalexander <riyaalexander@Riyas-MacBook-Air.local>
2023-05-22 10:31:57 -0400

    edits

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5500
 const errorHandler = require('./middleware/errorHandler.js')
-const db = require('./db');
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(errorHandler);
